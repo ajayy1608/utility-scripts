ca2a23f53660a913cc18fbf27da1cf87c0cb9031
aksy121 <aksy121@gmail.com>
2019-11-09 23:32:08 +0400

    adding pod counting script

---
+++ count_pods.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+total_pods=0
+nodes=$(kubectl get no -o=jsonpath="{range.items[*]}{.metadata.name} {end}")
+for node in $nodes;do
+  pods=$(kubectl get pods --all-namespaces --field-selector spec.nodeName="$node" | wc -l)
+  total_pods=$((total_pods + pods))
+  echo "Pods on $node: ${pods// /}"
+done
+echo "------------------------------------------------------------"
+echo "Total Pods: $total_pods"
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Utility Scripts
+
+The repository contains the utility scripts that are very simple but sometimes needed for quick action.
+
+### count_pods.sh
+This script finds out the nodes in your kubernetes cluster and counts the pods running on each node, to give you an idea of your workload distribution.
