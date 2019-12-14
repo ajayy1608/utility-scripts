655d49947b4da1be429b4d093502e6be6187fd0d
aksy121 <aksy121@gmail.com>
2019-12-14 11:05:27 +0400

    Adding no-headers option

--- count_pods.sh
+++ count_pods.sh
@@ -3,7 +3,7 @@
 total_pods=0
 nodes=$(kubectl get no -o=jsonpath="{range.items[*]}{.metadata.name} {end}")
 for node in $nodes;do
-  pods=$(kubectl get pods --all-namespaces --field-selector spec.nodeName="$node" | wc -l)
+  pods=$(kubectl get pods --all-namespaces --no-headers --field-selector spec.nodeName="$node" | wc -l)
   total_pods=$((total_pods + pods))
   echo "Pods on $node: ${pods// /}"
 done
